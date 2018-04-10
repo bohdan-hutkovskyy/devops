6f2645f0646e94fdfb57b656bb048e555645a5aa
Bohdan Hutkovskyy <bhutkovskyy@rapidsos.com>
2018-04-10 11:32:14 -0400

    change

--- git_change.sh
+++ git_change.sh
@@ -1,41 +1,35 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e 
 
-# disabling set -e and relying on explicitly defined exit codes due to expected
-# failures as negative tests
-#set -e
+PANTS_SERVICES="src/python/pants/services"
 
-bom_sniffer() { 
-  # exit code is 1 if character not found
-  head -c3 "$1" | LC_ALL=C grep -qe '\xef\xbb\xbf'; 
-  if [ $? -eq 0 ] 
-  then 
-    echo "BOM SNIFFER DETECTED BOM CHARACTER IN FILE \"$1\""
-    exit 1
-  fi
-}
-check_rc() {
-  # exit if passed in value is not = 0
-  # $1 = return code
-  # $2 = command / label
-  if [ $1 -ne 0 ]
-  then
-    echo "$2 command failed"
-    exit 1
-  fi
-}
+declare -a APP_ARRAY
+#git diff --name-only origin/master | sort -u | awk 'BEGIN {FS="src/python/pants/services"} {print $2}'
 
-# finding files that differ from this commit and master
-echo 'git fetch'
-check_rc $? 'echo git fetch'
-git fetch
-check_rc $? 'git fetch'
-echo 'git diff --name-only origin/master'
-check_rc $? 'echo git diff'
+#git diff --name-only origin/master | xargs
 diff_files=`git diff --name-only origin/master | xargs`
-check_rc $? 'git diff'
 for x in ${diff_files}
 do
-  #echo "${x}"
-  bom_sniffer "${x}"
-  check_rc $? "BOM character detected in ${x},"
+    #echo "${x}" | awk 'BEGIN {FS="src/python/pants/services"} {print $2}' | cut -d'/' -f3
+    APP=`echo "${x}" | awk 'BEGIN {FS="src/python/pants/services"} {print $2}' | cut -d'/' -f3` 
+    if [ -n "$APP" ]; then
+      #echo $APP
+      APP_ARRAY+=($APP)
+    fi
+
 done
+
+# echo $APP
+#echo $APP_ARRAY
+# echo ${!APP_ARRAY[@]}
+
+echo ${APP_ARRAY[@]}
+
+#Remove Duplicates
+APP_ARRAY=($(echo ${APP_ARRAY[@]} | tr ' ' '\n' | sort -nu))
+
+echo $APP_ARRAY
+# array=("item 1" "item 2" "item 3")
+# for i in "${array[@]}"; do   # The quotes are necessary here
+#     echo "$i"
+# done
